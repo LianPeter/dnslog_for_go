ce35b6899a22d7526e25cdda508d1089d531ff01
William <iswilliam.dev@gmail.com>
2025-04-12 17:47:27 +0800

    update README

--- static/dnslog.js
+++ static/dnslog.js
@@ -1,12 +1,12 @@
 window.onload = function () {
     document.getElementById('dnslog-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        const domain = document.getElementById('domain').value;
+        const domain = document.getElementById('domain_name').value;
 
         // 调试：查看传递的域名
         console.log(domain);
 
-        fetch(`http://localhost:5000/log_dns?domain=${domain}`)
+        fetch(`http://localhost:8080/log_dns?domain=${domain}`)
             .then(response => response.text())
             .then(data => {
                 document.getElementById('result').innerText = `查询结果: ${data}`;
