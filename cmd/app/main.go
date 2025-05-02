0fa48339fe9e98fc7f85abc253ab0ba342e81786
William <iswilliam.dev@gmail.com>
2025-05-02 18:39:10 +0800

    add recover default config code

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"dnslog_for_go/internal/domain/dns_server"
 	"dnslog_for_go/internal/init_conf"
 	"dnslog_for_go/internal/log_write"
 	"dnslog_for_go/internal/router"
@@ -11,4 +12,5 @@ func main() {
 	log_write.InitZapLogger() // 初始化日志
 	router.StartServer(web.EmbedFiles)
 	init_conf.IsExist()
+	dns_server.DefaultConfig() // 恢复默认配置
 }
